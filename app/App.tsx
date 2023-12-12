d9cba3669cff770bd0e77185e6f9b6cd155fd579
Hailes Maurício <hailes.antonio@mastercontas.com>
2023-12-12 14:27:58 +0000

    💄 update-statusbar

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter";
 import { Loading } from "./source/components/Loading";
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
 		</View>
 	);
 }
