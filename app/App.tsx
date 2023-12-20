23b14da6e9f68acf2b5894afd37d11edb634bfa2
Hailes Maurício <hailes.antonio@mastercontas.com>
2023-12-20 16:03:00 +0000

    ➕ nativewind

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,8 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter";
 import { Loading } from "./source/components/Loading";
+import { Home } from './source/pages/Home';
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
@@ -14,18 +15,10 @@ export default function App() {
 	}
 
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+		<>
+			<Home></Home>
 			<StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-		</View>
+		</>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
---
+++ app/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        backgroundColor: '#FFFFFF'
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    },
+  },
+  plugins: [],
+}
+
